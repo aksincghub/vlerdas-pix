343260dcf57b47d6a61f7e9de9f3bb01a6acfc5a
Julian Jewel <julian@julianjewel.com>
2013-10-13 20:23:15 -0700

    Added NIEM to PIX service

--- mock/server.js
+++ mock/server.js
@@ -13,6 +13,27 @@ module.exports.config = config;
 
 createApp();
 // Create Express App
+
+var identifier = '{'
+  +'"identifier:Identifier": {'
+    +'"-xmlns:s": "http://niem.gov/niem/structures/2.0",'
+    +'"-xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",'
+    +'"-xmlns:identifier": "http://vler.va.gov/vler/schemas/internal/identifier",'
+    +'"-xmlns:nc": "http://niem.gov/niem/niem-core/2.0",'
+    +'"-xmlns:vler": "http://va.gov/vler/schemas/vlerSupersetSchema/0.7/vler",'
+    +'"identifier:CommonData": {'
+      +'"vler:Client": {'
+        +'"-s:id": "client",'
+        +'"vler:ClientIdentifier": ['
+          +'{'
+            +'"nc:IdentificationID": "UNKNOWN",'
+            +'"vler:AssigningAuthority": "2.16.840.1.113883.3.275"'
+          +'}'
+		+']'
+	  +'}'
+    +'}'
+  +'}'
++'}';
 function createApp() {
     var app = express();
 
@@ -28,26 +49,55 @@ function createApp() {
 	// Get the patient identifier cross-reference
 	app.get('/2.16.840.1.113883.4.1/1234567', function (req, res, next) {
 		res.header('Content-Type', 'application/json');
-		res.send ('[{"2.16.840.1.113883.3.275":"7654321"}]', 200);
+		
+		var result = JSON.parse(identifier);
+		var jsonToAdd = JSON.parse( '{"nc:IdentificationID":"' + '7654321' + '", "vler:AssigningAuthority":"2.16.840.1.113883.3.275" }');
+		var clientIdentifierArray = [] ;
+		clientIdentifierArray[0] = jsonToAdd;
+		result['identifier:Identifier']['identifier:CommonData']['vler:Client']['vler:ClientIdentifier'] = clientIdentifierArray;
+		
+		var resultArr = result['identifier:Identifier']['identifier:CommonData']['vler:Client']['vler:ClientIdentifier'];
+		_.each(resultArr, function(result, index) {
+			console.log('Result ID' + result['nc:IdentificationID']);
+			console.log('Result AA' + result['vler:AssigningAuthority']);
+		})
+		
+		res.send (result, 200);
 	});
 
 		// Get the patient identifier cross-reference
 	app.get('/2.16.840.1.113883.4.1/123123123', function (req, res, next) {
 		res.header('Content-Type', 'application/json');
-		res.send ('[{"2.16.840.1.113883.3.275":"321321321"}]', 200);
+		var result = JSON.parse(identifier);
+		var jsonToAdd = JSON.parse( '{"nc:IdentificationID":"' + '321321' + '", "vler:AssigningAuthority":"2.16.840.1.113883.3.275" }');
+		var clientIdentifierArray = [] ;
+		clientIdentifierArray[0] = jsonToAdd;
+		result['identifier:Identifier']['identifier:CommonData']['vler:Client']['vler:ClientIdentifier'] = clientIdentifierArray;
+		
+		res.send (result, 200);
 	});
 
 	// Get the patient identifier cross-reference
 	app.get('/2.16.840.1.113883.4.1/1111111', function (req, res, next) {
 		res.header('Content-Type', 'application/json');
-		res.send('[{"2.16.840.1.113883.3.275":"MULTIPLE"}]', 200);
+		var result = JSON.parse(identifier);
+		var jsonToAdd = JSON.parse( '{"nc:IdentificationID":"' + 'MULTIPLE' + '", "vler:AssigningAuthority":"2.16.840.1.113883.3.275" }');
+		var clientIdentifierArray = [] ;
+		clientIdentifierArray[0] = jsonToAdd;
+		result['identifier:Identifier']['identifier:CommonData']['vler:Client']['vler:ClientIdentifier'] = clientIdentifierArray;
+		res.send (result, 200);
 	});
 
 
 		// Get the patient identifier cross-reference
 	app.get('/2.16.840.1.113883.4.1/:identifier', function (req, res, next) {
 		res.header('Content-Type', 'application/json');
-		res.send ('[{"2.16.840.1.113883.3.275":"UNKNOWN"}]', 200);
+		var result = JSON.parse(identifier);
+		var jsonToAdd = JSON.parse( '{"nc:IdentificationID":"' + 'UNKNOWN' + '", "vler:AssigningAuthority":"2.16.840.1.113883.3.275" }');
+		var clientIdentifierArray = [] ;
+		clientIdentifierArray[0] = jsonToAdd;
+		result['identifier:Identifier']['identifier:CommonData']['vler:Client']['vler:ClientIdentifier'] = clientIdentifierArray;
+		res.send (result, 200);
 	});
 
 	// Listen
