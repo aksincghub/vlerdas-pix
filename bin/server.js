d9c0600ef61b90f433b64be65a9f8cc428c3daa3
Julian Jewel <julian@julianjewel.com>
2013-10-27 19:51:45 -0700

    Added retry and cluster

--- bin/server.js
+++ bin/server.js
@@ -7,7 +7,7 @@
 var config = require('config');
 // Export config, so that it can be used anywhere
 var express = require('express')
-module.exports.config = config;
+    module.exports.config = config;
 var _ = require('underscore');
 var Log = require('vcommons').log;
 var logger = Log.getLogger('PIX', config.log);
@@ -27,6 +27,8 @@ var connectData = {
 // Generic pool for database connection
 var generic_pool = require('generic-pool');
 var https = require('https');
+var cluster = require("cluster");
+var numCPUs = require('os').cpus().length;
 
 logger.trace('Establishing pool with max:', config.db.pool);
 var pool = generic_pool.Pool({
@@ -55,23 +57,41 @@ var pool = generic_pool.Pool({
 
     });
 
-logger.info('Starting PIX Service...');
-createApp(pool);
+if (cluster.isMaster) {
+    // Fork workers.
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('online', function (worker) {
+        logger.info('A worker with #' + worker.id);
+    });
+    cluster.on('listening', function (worker, address) {
+        logger.info('A worker is now connected to ' + address.address + ':' + address.port);
+    });
+    cluster.on('exit', function (worker, code, signal) {
+        logger.info('worker ' + worker.process.pid + ' died');
+    });
+} else {
+    logger.info('Starting PIX Service...');
+    createApp(pool);
+}
 // Create Express App
 function createApp(pool) {
     var app = express();
 
     app.configure(function () {
+        // enable web server logging; pipe those log messages through winston
+        var winstonStream = {
+            write : function (message, encoding) {
+                logger.trace(message);
+            }
+        };
         // Log
-        app.use(express.logger());
+        app.use(express.logger({
+                stream : winstonStream
+            }));
         app.use(app.router);
-        // Simple Access Control - TODO: Preferences & Authorizations
-        if (config.accessControl) {
-            logger.trace('Setting up access control');
-            var accessControl = require('vcommons').accessControl;
-            app.use(accessControl());
-        }
-
         // Only for development
         if (config.debug) {
             app.use(express.errorHandler({
@@ -107,24 +127,22 @@ function createApp(pool) {
 
 }
 
+function fixOptions(configOptions) {
+    var options = {};
 
-function fixOptions(configOptions)
-{
-	var options = {};
+    if (!_.isUndefined(configOptions.key) && _.isString(configOptions.key)) {
+        options.key = fs.readFileSync(configOptions.key);
+    }
 
-	if (!_.isUndefined(configOptions.key) && _.isString(configOptions.key)) {
-		options.key = fs.readFileSync(configOptions.key);
-	}
+    if (!_.isUndefined(configOptions.cert) && _.isString(configOptions.cert)) {
+        options.cert = fs.readFileSync(configOptions.cert);
+    }
 
-	if (!_.isUndefined(configOptions.cert) && _.isString(configOptions.cert)) {
-		options.cert = fs.readFileSync(configOptions.cert);
-	}
+    if (!_.isUndefined(configOptions.pfx) && _.isString(configOptions.pfx)) {
+        options.pfx = fs.readFileSync(configOptions.pfx);
+    }
 
-	if (!_.isUndefined(configOptions.pfx) && _.isString(configOptions.pfx)) {
-		options.pfx = fs.readFileSync(configOptions.pfx);
-	}
-
-	return options;
+    return options;
 }
 // Default exception handler
 process.on('uncaughtException', function (err) {
